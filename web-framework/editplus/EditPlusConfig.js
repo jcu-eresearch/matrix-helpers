6cd11443d8a79cb8dd23f7c6469e7bb414761f61
David Beitey <david@davidjb.com>
2016-12-01 10:57:11 +1000

    Customise EditPlus config for our purposes

--- web-framework/editplus/EditPlusConfig.js
+++ web-framework/editplus/EditPlusConfig.js
@@ -4,42 +4,40 @@
  *  More help on EES install and implementatuon can be found at http://manuals.matrix.squizsuite.net/ees/chapters/installation/
  */
 var EasyEditConfig = {
-    
+
     // Internal debugging flag. Setting this value to true will log debugging data
     // to the console in browsers that support console.log.
     // Quietly ignored in browsers that do not support console.log.
     debug:                      false,
-    
+
     // The title bar that is displayed for EES.
     // @asset_name@ is a dynamic replacement for the current assets name.
-    titleBar:                   "@asset_name@ : Squiz Matrix: Edit+",
-    
+    titleBar:                   "@asset_name@ : James Cook University Edit+",
+
     // Lock refresh interval in seconds.
     // EES automatically refreshed all locks for an asset based on this value.
     lockRefreshInterval:        120,
-    
+
     // Array of base site urls. (Replaces 'eesSiteURL' global variable from EES builds 832 and 833)
     // For the majority systems, this can stay empty.
     // If required, the array of URLs should match up with urls applied in the Squiz Matrix system configuration.
     // If this is left empty or EES is installed on a site url that doesn't match a base site url then
     // a domain relative url will be used.
     baseSiteUrls: [],
-    
+
     // JS API configuration options.
     // Populate with the API key found on the EES related JS API details screen.
     JSAPI: {
         key:                    "1881698051"
     },
-    
 
-    
     // Cascade status change threshold.
-    // If a user selects 'Cascade Status Change' on the Details Screen and the 
+    // If a user selects 'Cascade Status Change' on the Details Screen and the
     // total number of child assets exceeds this value they will be
     // presented with a prompt to continue or to return to the screen.
     cascadeThreshold:           4,
 
-    
+
     // Timeout to show a warning for overlays that are active for more than X number of
     // seconds. This is usually because of an ajax function that has for whatever
     // reason failed to return successfully and could indicate server error.
@@ -76,12 +74,11 @@ var EasyEditConfig = {
     // If missing from config pagination will default to 100.
     // 0 = no pagination.
     assetFinderMaxAssets:         100,
-    
+
     // PERFORMANCE FEATURE.
     // Cache manager default expiry, minutes expressed as a whole number.
     cacheManagerDefaultExpiry:    2,
 
-    
     // Show 'Set Thumbnail' section on details screen.
     // Adds the ability to add/remove/update a thumbnail to all assets.
     // If missing from config thumbnail will show by default, set to false to hide.
@@ -89,15 +86,15 @@ var EasyEditConfig = {
 
     // Display and allow editing of future statuses via the details screen.
     // If missing from config or false the details screen will not display 'Show Future Status'.
-    allowFutureStatusChange:     false,
+    allowFutureStatusChange:     true,
 
     // PERFORMANCE FEATURE.
     // Display a list of direct children on the link screen.
     // If missing from config or false the linking screen will not display 'Direct Children'.
     showChildrenOnLinkingScreen: true,
-    
+
     //GOOGLE ANALYTICS CONNECTOR
     // A new Analytics screen will be made available for your assets, displaying the analytics data of your assets.
-    analyticsViewId:             318119 
-       
+    analyticsViewId:             318119
+
 }; // End EasyEditConfig.
