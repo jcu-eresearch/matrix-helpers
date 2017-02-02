b47d238562558c5922ec49902a0de485c9d2689b
David Beitey <david@davidjb.com>
2017-02-02 17:49:32 +1000

    Switch breadcrumbs to using Server-side JS

---
+++ web-framework/js/breadcrumbs.js
@@ -0,0 +1,28 @@
+// Only show breadcrumbs if enabled and on pages that aren't the homepage
+var data = {}
+data.site_index_id = '%globals_site_index_id%'
+data.site_breadcrumbs = '%globals_site_metadata_jcu.features.breadcrumbs%'
+data.frontend_id = '%frontend_asset_assetid%'  // This should be corrected for index pages
+data.frontend_breadcrumbs = '%frontend_asset_metadata_jcu.features.breadcrumbs%'
+
+if (data.frontend_breadcrumbs === 'true' ||
+    data.frontend_breadcrumbs === 'inherit' && data.site_breadcrumbs) {
+  if (data.site_index_id !== data.frontend_id) {
+    var lineage = %globals_frontend_asset_linking_lineage%
+    if (lineage) {
+      print(
+        '<div class="container jcu-content">' +
+          '<nav class="breadcrumb jcu-bg--transparent">')
+      _.each(lineage, function(id) {
+        if (id !== data.frontend_id) {
+          print('<a class="breadcrumb-item" href="%globals_asset_url:' + id + '%">%globals_asset_short_name:' + id + '%</a>')
+        } else {
+          print('<span class="breadcrumb-item active">%globals_asset_short_name:' + id + '%</span>')
+        }
+      })
+      print(
+          '</nav>' +
+        '</div>')
+    }
+  }
+}
