96f1d0a76fdb6fb450aefa95713f8c6516096a96
David Beitey <david@davidjb.com>
2017-02-28 14:30:04 +1000

    Refactor data and functions into JCU.* namespace

--- web-framework/js/breadcrumbs.js
+++ web-framework/js/breadcrumbs.js
@@ -1,22 +1,22 @@
 /* jslint asi: true */
-/* globals _, print, data */
+/* globals _, print, JCU */
 
 // Only show breadcrumbs if enabled and on pages that aren't the homepage
-data.site_index_id = '%globals_site_index_id%'
-data.site_breadcrumbs = '%globals_site_metadata_jcu.features.breadcrumbs%'
-data.frontend_id = '%frontend_asset_assetid%'  // This should be corrected for index pages
-data.frontend_breadcrumbs = '%frontend_asset_metadata_jcu.features.breadcrumbs%'
+JCU.data.site_index_id = '%globals_site_index_id%'
+JCU.data.site_breadcrumbs = '%globals_site_metadata_jcu.features.breadcrumbs%'
+JCU.data.frontend_id = '%frontend_asset_assetid%'  // This should be corrected for index pages
+JCU.data.frontend_breadcrumbs = '%frontend_asset_metadata_jcu.features.breadcrumbs%'
 
-if (data.frontend_breadcrumbs === 'true' ||
-    data.frontend_breadcrumbs === 'inherit' && data.site_breadcrumbs) {
-  if (data.site_index_id !== data.frontend_id) {
+if (JCU.data.frontend_breadcrumbs === 'true' ||
+    JCU.data.frontend_breadcrumbs === 'inherit' && JCU.data.site_breadcrumbs) {
+  if (JCU.data.site_index_id !== JCU.data.frontend_id) {
     var lineage = %frontend_asset_linking_lineage%
     if (lineage) {
       print(
         '<div class="container jcu-content">' +
           '<nav class="breadcrumb jcu-bg--transparent">')
       _.each(lineage, function(id) {
-        if (id !== data.frontend_id) {
+        if (id !== JCU.data.frontend_id) {
           print('<a class="breadcrumb-item" href="%globals_asset_url:' + id + '%">%globals_asset_short_name:' + id + '%</a>')
         } else {
           print('<span class="breadcrumb-item active">%globals_asset_short_name:' + id + '%</span>')
--- web-framework/js/ssjs-config.js
+++ web-framework/js/ssjs-config.js
@@ -10,28 +10,57 @@ if (typeof window !== "undefined") {
     _.forEach(arguments, function(val) { document.write(JSON.stringify(val)) } )
   }
 }
-var data = {
-  includes: {
+
+/* Public API for JCU SSJS functions */
+var JCU = {
+  // Data and information used in rendering
+  data: {},
+
+  // Libraries to be included later
+  _includes: {
     css: [],
     js: []
+  },
+
+  js: {
+    require: function(name) {
+      this._includes.js[name] = true
+    },
+    specified: function(name) {
+      return !!this._includes.js[name]
+    }
+  },
+
+  css: {
+    require: function(name) {
+      this._includes.css[name] = true
+    },
+    specified: function(name) {
+      return !!this._includes.css[name]
+    }
+  },
+
+  phrasingHtmlFromKeyword: function(keyword, options) {
+    options = options || {}
+    var strip_modifier = 'striphtml:<abbr><audio><b><bdo><br><button><canvas><cite><code><command><datalist><dfn><em><embed><i><iframe><img><input><kbd><keygen><label><mark><math><meter><noscript><object><output><progress><q><ruby><samp><script><select><small><span><strong><sub><sup><svg><textarea><time><var><video><wbr><a><area><del><ins><link><map><meta>'
+
+    if (keyword) {
+      return '%' + keyword + '^' + strip_modifier +
+        (options.prepend ? '^prepend_if:' + options.prepend : '') +
+        (options.append ? '^append_if:' + options.append : '') + '%'
+    } else {
+      print('<div class="alert alert-danger" role="alert"><strong>Error!</strong> Phrasing HTML paint layout called without a keyword specified.</div>')
+    }
+  },
+
+  printPhrasingHtmlFromKeyword: function(keyword, options) {
+    print(this.phrasingHtmlFromKeyword(keyword, options))
   }
 }
 
-function phrasingHtmlFromKeyword(keyword, options) {
-  options = options || {}
-  var strip_modifier = 'striphtml:<abbr><audio><b><bdo><br><button><canvas><cite><code><command><datalist><dfn><em><embed><i><iframe><img><input><kbd><keygen><label><mark><math><meter><noscript><object><output><progress><q><ruby><samp><script><select><small><span><strong><sub><sup><svg><textarea><time><var><video><wbr><a><area><del><ins><link><map><meta>'
+// BBB Compatibility
+phrasingHtmlFromKeyword = JCU.phrasingHtmlFromKeyword
+printPhrasingHtmlFromKeyword = JCU.printPhrasingHtmlFromKeyword
 
-  if (keyword) {
-    return '%' + keyword + '^' + strip_modifier +
-      (options.prepend ? '^prepend_if:' + options.prepend : '') +
-      (options.append ? '^append_if:' + options.append : '') + '%'
-  } else {
-    print('<div class="alert alert-danger" role="alert"><strong>Error!</strong> Phrasing HTML paint layout called without a keyword specified.</div>')
-  }
-}
-
-function printPhrasingHtmlFromKeyword(keyword, options) {
-  print(phrasingHtmlFromKeyword(keyword, options))
-}
 
 // Add jcu function execution here...
