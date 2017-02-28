ce965e64454d4bda2ec0e5bee2872b9ea3e974df
David Beitey <david@davidjb.com>
2017-02-28 11:26:46 +1000

    Switch to lodash on SSJS

--- web-framework/js/ssjs-config.js
+++ web-framework/js/ssjs-config.js
@@ -4,10 +4,10 @@
 // If in a browser (via ?SQ_DISABLE_SERVER_JS), re-work the print() function
 if (typeof window !== "undefined") {
   window.print = function() {
-    _.each(arguments, function(val) { document.write('' + val)  } )
+    _.forEach(arguments, function(val) { document.write('' + val)  } )
   }
   window.var_dump = function() {
-    _.each(arguments, function(val) { document.write(JSON.stringify(val)) } )
+    _.forEach(arguments, function(val) { document.write(JSON.stringify(val)) } )
   }
 }
 var data = {
